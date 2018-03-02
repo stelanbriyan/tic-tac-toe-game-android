40ee5e94a8df65881e9f6c1987a24dbe75f38c07
stelan <stelan@sp-solutions.biz>
2018-03-02 18:04:47 +0530

    lecture 4

--- app/src/main/java/org/incosyz/stelan/tictactoe/GameActivity.java
+++ app/src/main/java/org/incosyz/stelan/tictactoe/GameActivity.java
@@ -1,14 +1,58 @@
 package org.incosyz.stelan.tictactoe;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
-    public static String KEY_RESTORE = "";
+    public static final String KEY_RESTORE = " key_restore ";
+    public static final String PREF_RESTORE = " pref_restore ";
+    private GameFragment mGameFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        mGameFragment = (GameFragment) getFragmentManager().findFragmentById(R.id.fragment_game);
+        boolean restore = getIntent().getBooleanExtra(KEY_RESTORE,
+                false);
+        if (restore) {
+            String gameData = getPreferences(MODE_PRIVATE)
+                    .getString(PREF_RESTORE, null);
+            if (gameData != null) {
+                mGameFragment.putState(gameData);
+            }
+        }
+    }
+
+
+    public void reportWinner(final Tile.Owner winner) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.declare_winner, winner));
+        builder.setCancelable(false);
+        builder.setPositiveButton(R.string.ok_label,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+        final Dialog dialog = builder.create();
+        dialog.show();
+        mGameFragment.initGame();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        String gameData = mGameFragment.getState();
+        getPreferences(MODE_PRIVATE).edit()
+                .putString(PREF_RESTORE, gameData)
+                .commit();
+    }
+
+    public void restartGame() {
+        mGameFragment.restartGame();
     }
 }
--- app/src/main/java/org/incosyz/stelan/tictactoe/GameFragment.java
+++ app/src/main/java/org/incosyz/stelan/tictactoe/GameFragment.java
@@ -1,20 +1,40 @@
 package org.incosyz.stelan.tictactoe;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Stelan Briyan
  */
 public class GameFragment extends Fragment {
-    // Data structures go here ...
+
+    static private int mLargeIds[] = {R.id.large1, R.id.large2,
+            R.id.large3, R.id.large4, R.id.large5,
+            R.id.large6, R.id.large7, R.id.large8,
+            R.id.large9};
+    static private int mSmallIds[] = {R.id.small1, R.id.small2,
+            R.id.small3, R.id.small4, R.id.small5,
+            R.id.small6, R.id.small7, R.id.small8,
+            R.id.small9};
+    private Tile mEntireBoard = new Tile(this);
+    private Tile mLargeTiles[] = new Tile[9];
+    private Tile mSmallTiles[][] = new Tile[9][9];
+    private Tile.Owner mPlayer = Tile.Owner.X; // whose turn is it?
+    private Set<Tile> mAvailable = new HashSet<>();
+    private int mLastLarge;
+    private int mLastSmall;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        initGame();
     }
 
     public View onCreateView(LayoutInflater inflater,
@@ -22,7 +42,164 @@ public class GameFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.large_board,
                 container, false);
-
+        initViews(rootView);
+        updateAllTiles();
         return rootView;
+    }
+
+    public void initGame() {
+        mEntireBoard = new Tile(this);
+// Create all the tiles
+        for (int large = 0; large < 9; large++) {
+            mLargeTiles[large] = new Tile(this);
+            for (int small = 0; small < 9; small++) {
+                mSmallTiles[large][small] = new Tile(this);
+            }
+            mLargeTiles[large].setSubTiles(mSmallTiles[large]);
+        }
+        mEntireBoard.setSubTiles(mLargeTiles);
+// make all the moves available initially
+        mLastSmall = -1;
+        mLastLarge = -1;
+        setAvailableFromLastMove(mLastSmall);
+    }
+
+    private void setAvailableFromLastMove(int small) {
+        clearAvailable();
+// Make all the tiles at the destination available
+        if (small != -1) {
+            for (int dest = 0; dest < 9; dest++) {
+                Tile tile = mSmallTiles[small][dest];
+                if (tile.getOwner() == Tile.Owner.NEITHER)
+                    addAvailable(tile);
+            }
+        }
+/* If there were none available in the destination board ,
+mark all non - occupied squares in the other boards as
+available */
+        if (mAvailable.isEmpty()) {
+            setAllAvailable();
+        }
+    }
+
+    private void setAllAvailable() {
+        for (int large = 0; large < 9; large++) {
+            for (int small = 0; small < 9; small++) {
+                Tile tile = mSmallTiles[large][small];
+                if (tile.getOwner() == Tile.Owner.NEITHER)
+                    addAvailable(tile);
+            }
+        }
+    }
+
+    private void clearAvailable() {
+        mAvailable.clear();
+    }
+
+    private void addAvailable(Tile tile) {
+        mAvailable.add(tile);
+    }
+
+    public boolean isAvailable(Tile tile) {
+        return mAvailable.contains(tile);
+    }
+
+    private void makeMove(int large, int small) {
+        mLastLarge = large;
+        mLastSmall = small;
+        Tile smallTile = mSmallTiles[large][small];
+        Tile largeTile = mLargeTiles[large];
+        smallTile.setOwner(mPlayer);
+        setAvailableFromLastMove(small);
+        Tile.Owner oldWinner = largeTile.getOwner();
+        Tile.Owner winner = largeTile.findWinner();
+        if (winner != oldWinner) {
+            largeTile.setOwner(winner);
+        }
+        winner = mEntireBoard.findWinner();
+        mEntireBoard.setOwner(winner);
+        updateAllTiles();
+        if (winner != Tile.Owner.NEITHER) {
+            ((GameActivity) getActivity()).reportWinner(winner);
+        }
+    }
+
+    private void initViews(View rootView) {
+        mEntireBoard.setView(rootView);
+        for (int large = 0; large < 9; large++) {
+            View outer = rootView.findViewById(mLargeIds[large]);
+            mLargeTiles[large].setView(outer);
+            for (int small = 0; small < 9; small++) {
+                ImageButton inner = (ImageButton) outer.findViewById
+                        (mSmallIds[small]);
+                final int fLarge = large;
+                final int fSmall = small;
+                final Tile smallTile = mSmallTiles[large][small];
+                smallTile.setView(inner);
+                inner.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (isAvailable(smallTile)) {
+                            makeMove(fLarge, fSmall);
+                            switchTurns();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void switchTurns() {
+        mPlayer = mPlayer == Tile.Owner.X ? Tile.Owner.O : Tile.Owner.X;
+    }
+
+    private void updateAllTiles() {
+        mEntireBoard.updateDrawableState();
+        for (int large = 0; large < 9; large++) {
+            mLargeTiles[large].updateDrawableState();
+            for (int small = 0; small < 9; small++) {
+                mSmallTiles[large][small].updateDrawableState();
+            }
+        }
+    }
+
+    public String getState() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(mLastLarge);
+        builder.append(",");
+        builder.append(mLastSmall);
+        builder.append(",");
+        for (int large = 0; large < 9; large++) {
+            for (int small = 0; small < 9; small++) {
+                builder.append(
+                        mSmallTiles[large][small].getOwner().name());
+                builder.append(",");
+            }
+        }
+        return builder.toString();
+    }
+
+    public void restartGame() {
+        initGame();
+        initViews(getView());
+        updateAllTiles();
+    }
+
+    /**
+     * Restore the state of the game from the given string .
+     */
+    public void putState(String gameData) {
+        String[] fields = gameData.split(" ,");
+        int index = 0;
+        mLastLarge = Integer.parseInt(fields[index++]);
+        mLastSmall = Integer.parseInt(fields[index++]);
+        for (int large = 0; large < 9; large++) {
+            for (int small = 0; small < 9; small++) {
+                Tile.Owner owner = Tile.Owner.valueOf(fields[index++]);
+                mSmallTiles[large][small].setOwner(owner);
+            }
+        }
+        setAvailableFromLastMove(mLastSmall);
+        updateAllTiles();
     }
 }
